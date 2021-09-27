fcc094072a0c12d7f1cd0b3124993d7920b1618c
Deluxe <giobibiluri.gb@gmail.com>
2021-09-28 01:39:05 +0400

    feat(cart) - added Sum of products, current product sum and ability to clear shopping cart

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,25 +1,37 @@
 import { CartService } from './../services/cart.service';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.scss']
 })
-export class ShoppingCartComponent implements OnInit {
+export class ShoppingCartComponent {
   constructor(private cartService: CartService) { }
   get products() {
     return this.cartService.cartProducts;
   }
 
+  get totalItems (): number {
+     return this.cartService.getLength();
+  } 
+
   updateAmount(id: string | undefined, amount: number) {
     this.cartService.updateAmount(id, amount); 
   }
 
-  totalItems: number = this.cartService.getLength();
-
-  ngOnInit(): void {
-
+  totalPrice(): number {
+    return this.cartService.cartProducts.map(p => p.product.price * p.amount).reduce((acc, curr) => acc + curr, 0);
   }
+
+  currentTotal(i: number): number {
+    return this.cartService.cartProducts[i].amount * this.cartService.cartProducts[i].product.price;
+  }
+
+  clearShoppingCart() {
+    this.cartService.cartProducts.splice(0, this.cartService.cartProducts.length);
+  }
+
+  
 
 }
