cbd4a2cab8ee3fa7e5f73584fff6bc4a50d4aaa9
Deluxe <giobibiluri.gb@gmail.com>
2021-09-27 23:33:35 +0400

    feat(products) - added filter

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,9 @@
+import { switchMap, map } from 'rxjs/operators';
 import { CategoryService } from './../services/category.service';
 import { ProductsService } from './../services/products.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-home',
@@ -8,14 +11,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  activatedRoute: string = 'all'
+
   constructor(
     private categoryService: CategoryService,
-    private productsService: ProductsService) { }
-  products$ = this.productsService.fetchProducts()
+    private productsService: ProductsService,
+    private route: ActivatedRoute,
+    private router: Router
+    ) { }
+  products: Product[] = [];
   categories$ = this.categoryService.fetchCategories()
 
   ngOnInit(): void {
-    
+    this.route.queryParams
+    .pipe(
+      switchMap(param => this.productsService.fetchProducts().pipe(
+        map(products => {
+          if(param?.cat){
+            this.activatedRoute = param.cat
+            return products.filter(product => product.category === param.cat);
+          }else {
+            this.activatedRoute = 'all';
+            return products;
+          }
+        })
+      ))
+    ).subscribe(filteredProducts => this.products = filteredProducts)
+  }
+
+  filterByCat(category: string) {
+    if(category) {
+      this.router.navigate([], {
+        queryParams: {
+          cat: category
+        }
+      })
+    }else {
+      this.router.navigate([], {
+        queryParams: {
+        }
+      })
+    }
   }
 
 }
